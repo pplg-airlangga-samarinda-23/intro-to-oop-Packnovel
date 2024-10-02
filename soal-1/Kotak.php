2d49bbed003e71ea35d30c9c4e517f3b853a3243
lalu adam <adamlalu22@gmail.com>
2024-10-02 23:10:30 +0800

    ini kenapa cmn ke commit 1 ya

--- soal-1/Kotak.php
+++ soal-1/Kotak.php
@@ -2,24 +2,53 @@
 class PersegiPanjang
 {
     // deklarasikan panjang dan lebar
+    private $panjang;
+    private $lebar;
 
 
     // deklarasikan metode konstruktor
+    public function __construct($panjang, $lebar)
+    {
+        $this->panjang = $panjang;
+        $this->lebar = $lebar;
+    }
 
 
     // deklarasikan metode mutator dan asesor
+    public function setPanjang($panjang)
+    {
+        $this->panjang = $panjang;
+    }
+    public function setLebar($lebar)
+    {
+        $this->lebar = $lebar;
+    }
+    public function getPanjang()
+    {
+        return $this->panjang;
+        
+            }        public function getLebar()
+    {
+        return $this->lebar;
+    }
+
+       
+       
+
 
 
 
     public function hitungLuas()
     {
         // lengkapi kode untuk menghitung luas persegi panjang
+        return $this->panjang * $this->lebar;
 
     }
 
     public function hitungKeliling()
     {
         // lengkapi dengan kode untuk menghitung keliling persegi panjang
+        return   ($this->panjang + $this->lebar);
 
     }
 }
